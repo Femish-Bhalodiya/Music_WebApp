edf2b9abc08cf41b50810762203ef2d8d2df38be
BFemish <femish.bhalodiya7@gmail.com>
2022-09-27 23:38:05 +0530

    First Commit

---
+++ src/data.jsx
@@ -0,0 +1,261 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Warriyo - Mortals",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FMortals%20(feat.%20Laura%20Brehm).jpg?alt=media&token=9643a567-5e02-4939-b3d4-7a2ca2c7457b",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FWarriyo%20-%20Mortals%20(feat.%20Laura%20Brehm)%20%5BNCS%20Release%5D.mp3?alt=media&token=430b6f1f-0010-447c-8d6f-1c992c63923c",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Superhero",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FUnknown%20Brain%20-%20Superhero%20(feat.%20Chris%20Linton)%20%5BNCS%20Release%5D.jpg?alt=media&token=48bb9233-3f96-48c3-bd42-a7f42d180e52",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FUnknown%20Brain%20-%20Superhero%20(feat.%20Chris%20Linton)%20%5BNCS%20Release%5D.mp3?alt=media&token=25bcf391-11ba-4c9b-9910-b7d11ba9cd2a",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Fearless",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FFearless%20pt.%20II%20(feat.%20Chris%20Linton).jpg?alt=media&token=69303426-d32d-4920-a6d6-32a0afbb6461",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FTULE%20-%20Fearless%20pt.II%20(feat.%20Chris%20Linton)%20%5BNCS%20Release%5D.mp3?alt=media&token=c65104d1-e0ad-410f-85d0-f3775331eb51",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Survive",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FAxel%20Oliver%20x%20THEBOYWITHSPEC%20-%20Survive%20%5BNCS%20Release%5D.jpg?alt=media&token=064695eb-ee62-4338-b4e5-9503430a01a9",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FAxel%20Oliver%20x%20THEBOYWITHSPEC%20-%20Survive%20%5BNCS%20Release%5D.mp3?alt=media&token=a4d8779b-a214-45b5-8635-b690ff6082a4",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Don't know why",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FArya%20-%20Don't%20Know%20Why%20%5BNCS%20Release%5D.jpg?alt=media&token=8c3ced48-a2dc-45e6-839e-0f772e5d4c0e",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FArya%20-%20Don't%20Know%20Why%20%5BNCS%20Release%5D.mp3?alt=media&token=e961cc14-c087-4fb6-be03-da6dccbec34a",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "On & On",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2Fon%20%26%20on.jpg?alt=media&token=49cbff83-812b-4125-9ee5-67b744a9bcab",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FCartoon%20-%20On%20%26%20On%20(Feat.%20Daniel%20Levi)%20(nuumi%20Remix)%20%5BNCS%20Release%5D.mp3?alt=media&token=3cd5d941-91cd-4b0b-970e-b2a4ae8afe8e",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Blame it on me",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FBlame%20it%20on%20mejpg.jpg?alt=media&token=6a0ee899-916c-4f85-9e62-47c7a6197376",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FCADMIUM%20%26%20Shiah%20Maisel%20-%20Blame%20It%20On%20Me%20%5BNCS%20Release%5D.mp3?alt=media&token=72e3e1a1-0a63-4b60-8243-d9fc5b2c69c0",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Somebody Like Me",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FSunshine.jpg?alt=media&token=9214f3bf-173b-47ab-83f9-e35ad3584647",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FJJD%20%26%20Division%20One%20-%20Somebody%20Like%20Me%20(feat.%20Halvorsen)%20%5BNCS%20Release%5D.mp3?alt=media&token=cb133d34-38ed-45a7-8797-5214c9050bc6",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Sunshine",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FSomebody%20like%20me.jpg?alt=media&token=84da27c1-9035-41d3-a467-07d9a3691e08",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FGelow%20%26%20BPRTS%20-%20Sunshine%20%5BNCS%20Release%5D.mp3?alt=media&token=3a20ab6b-ffcf-4c62-8cc7-cdaa913f59da",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Would you be waiting",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FWould%20you%20be%20waiting.jpg?alt=media&token=f231c7cf-a92e-4a7d-8638-242e3a1e9f7a",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FAeden%20-%20Would%20You%20Be%20Waiting%20%5BNCS%20Release%5D.mp3?alt=media&token=c6f1f3fc-32f7-4fec-947b-2724be1e5b69",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Moments",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FMomentsjpg.jpg?alt=media&token=c2379c21-bad5-4a70-b17b-84a1870e10d5",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FLost%20Identities%20x%20Robbie%20Rosen%20-%20Moments%20(Acoustic%20Version)%20%5BNCS%20Release%5D.mp3?alt=media&token=c0522883-f638-46ee-b414-3f1a7456d721",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Believe",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FBelieve%20me.jpg?alt=media&token=24bcdeb1-b335-4fc3-bfba-a0e1cc21bffc",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2F_CADMIUM%20X%20JAMZ%20X%20SIMONNE%20-%20Believe%20Me%20%5BNCS%20Release%5D.mp3?alt=media&token=ffeed84d-3912-4bcf-9c2c-407566bb5c36",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Cool",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FCool.jpg?alt=media&token=6bfa64a8-230c-426e-88cc-168b34adf7c4",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FMagnus%20x%20Unread%20-%20Cool%20(feat.%20Alessia%20Labate)%20%5BNCS%20Release%5D.mp3?alt=media&token=cc3e9ad1-45a2-43fe-9911-33a6e48ac0b3",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Hunted House",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FHunted%20House.jpg?alt=media&token=e81b30a3-762f-4d52-9d51-d65e480a5106",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FNeoni%20-%20Haunted%20House%20%5BNCS%20Release%5D.mp3?alt=media&token=024d7b77-9e5c-44a5-a0bf-f6cd1f284915",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Last thing",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FLast%20Thing.jpg?alt=media&token=869d89c3-4487-45bb-b81a-cf506067795a",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FUnknown%20Brain%20-%20Last%20Thing%20(ft.%20Charlotte%20Sands)%20%5BNCS%20Release%5D.mp3?alt=media&token=29281d1a-86b7-45a1-ba70-a691c9b49df9",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "FreeFall",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FFreeFall.jpg?alt=media&token=73b168f2-a5a9-4c00-b922-3c60523eb43d",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FArlow%20-%20Freefall%20%5BNCS%20Release%5D.mp3?alt=media&token=c074e486-ce6c-4a3e-acb1-c5c899938e24",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Escape",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FEscape.jpg?alt=media&token=319e7b33-2526-4843-877a-b2ca428f3dbc",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FKeerthin%20-%20Escape%20%5BNCS%20Release%5D.mp3?alt=media&token=76b658ca-92af-40bf-b544-b5f3c51057c7",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Oh Darling",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FOh%20darling.jpg?alt=media&token=94e3c905-9dd1-49ff-b90b-f6076d793ddd",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FUnknown%20Brain%20-%20Oh%20Darling%20%5BNCS%20Release%5D.mp3?alt=media&token=4eca3d09-c910-4de3-9e46-696bf1b12ca3",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Need Ya",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FNeed%20Ya.jpg?alt=media&token=c66764e3-aed8-4e1f-a538-dc5dfc0d1f96",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FSyn%20Cole%20-%20Need%20Ya%20%5BNCS%20Release%5D.mp3?alt=media&token=6bc444e5-fbbe-4f52-85ad-4f58cc08b47b",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "How do you know",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FHow%20do%20you%20know.jpg?alt=media&token=75c4ff83-80fe-4bee-9f8a-80370666fa6f",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FArlow%20-%20How%20Do%20You%20Know%20%5BNCS%20Release%5D.mp3?alt=media&token=f00c0dff-8692-4dd8-bd60-8526f1aeb424",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Talk",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FTalk.jpg?alt=media&token=de46ee87-88b5-4d83-a2e0-a8140f26f1af",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FBeave%20-%20Talk%20%5BNCS%20Release%5D.mp3?alt=media&token=1a928697-4798-427c-b22b-d9dd80e22b43",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Everyday",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FEveryday.jpg?alt=media&token=28ee1153-5e15-4d14-9ac5-689878b3120f",
+      artist: "NCS",
+      audio:
+        "hhttps://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FBeatcore%20%26%20Ashley%20Apollodor%20-%20Everyday%20%5BNCS%20Release%5D.mp3?alt=media&token=03116d0f-c87a-4715-8680-d2c85ba1e1da",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Near x far",
+      cover:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/images%2FNear%20x%20Far.jpg?alt=media&token=9675e4e6-2aa2-475e-a14e-1c583a0ce7dc",
+      artist: "NCS",
+      audio:
+        "https://firebasestorage.googleapis.com/v0/b/music-app-cef88.appspot.com/o/musics%2FNear%20x%20Far%20-%20Not%20Enough%20%5BNCS%20Release%5D.mp3?alt=media&token=89709e71-5878-411a-8058-49e26c9066ac",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
